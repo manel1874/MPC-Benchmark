4fd9822e1c556608988e9623daf9d5f8d9b389cd
manel1874 <manel1874@gmail.com>
2020-10-15 14:47:23 +0100

    Updated Bob and Charlie

--- SemiHonestYao/gbmc-gc/PrivPhyloSys_2/Charlie/include/HamParties.hpp
+++ SemiHonestYao/gbmc-gc/PrivPhyloSys_2/Charlie/include/HamParties.hpp
@@ -3,16 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fstream>
+#include <boost/filesystem.hpp>
 
-#include "/home/manel/libscapi/include/comm/Comm.hpp"
-#include "/home/manel/libscapi/include/infra/ConfigFile.hpp"
+#include "../../../../../../libscapi/include/comm/Comm.hpp"
+#include "../../../../../../libscapi/include/infra/ConfigFile.hpp"
 
+string preprocessInput(string path);
+int hammingDistance(string input_i, string input_j);
 
 class HamParty
 {
 private:
-
-
 
 public:
     HamParty(int id, int numOfParties, int numOfInputs);
@@ -21,14 +22,24 @@ public:
     int numOfParties;
     int numOfInputs;
 
+    vector<int> numOfInputsOtherParties;
+
     //ConfigFile configFile;
     
     vector<int> ports;
     vector<string> ips;
 
-    vector<int> listOfRoles;
+    vector<vector<int>> results;
+
+    void runNumberOfInputs();
 
     void runHamSMC();
+
+    void evaluatorSendResultToGarbler();
+
+    void computeInternalHammingDistance();
+
+    void sendAndReceiveOtherDistances();
 
     //vector<vector<int>> mat(int numOfcols, vector<int>(numOfrows)); // Definir a matrix!
 
