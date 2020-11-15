5f945c5ee02757da79ff1150a676dbf23febd92a
manel1874 <manel1874@gmail.com>
2020-11-15 17:35:59 +0000

    Creating new boolean circuit with 1000 elements

--- SemiHonestYao/privPhyloTree/Bob/boolCircuit/main.c
+++ SemiHonestYao/privPhyloTree/Bob/boolCircuit/main.c
@@ -1,28 +1,10 @@
-#define LEN_SEQ 12
+#define LEN_SEQ 1000
 
 typedef unsigned char uint8_t;
 
 typedef struct {
 	uint8_t el[LEN_SEQ];
 } Array_Seq;
-
-
-//
-//int hammingDistance(Array_Seq array_A, Array_Seq array_B)
-//{
-
-//	int distance = 0;
-
-//	for(int i=0; i<LEN_SEQ; i++)
-//	{
-//		if(array_A.el[i] != array_B.el[i])
-//		{
-//			distance = distance + 1;
-//		}
-//	}
-
-//	return distance;
-//}
 
 
 
